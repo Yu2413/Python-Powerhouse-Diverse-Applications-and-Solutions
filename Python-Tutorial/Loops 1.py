e6ac0b448d73ba569cc7b565ab69972d9f7c6987
Gary Yu <143667967+Yu2413@users.noreply.github.com>
2025-04-06 19:37:18 -0400

    Add files via upload

---
+++ Python-Tutorial/Loops 1.py
@@ -0,0 +1,40 @@
+# This works too...
+# Word = "Hello"
+# for w in "Hello":
+#    print(w)
+
+Word = "Hello"
+
+Letters = []
+
+# for w in Word:
+#    print(w)
+#    if w == "e":
+#        print("What a funny letter")
+#    Letters.append(w)
+    # ['H','E','L','L','O']
+
+#print(Letters)
+
+#Numbers = [1, 2, 3, 4, 5]
+# 1 % 2 = 1
+# 2 % 2 = 0
+# 3 % 2 = 1
+
+#for l in Numbers:
+#    if l % 2 == 1:
+#        print(l)
+
+#range(start value,stopping value,steps)
+Numbers = []
+
+for num in range(-1,12,2):
+    Numbers.append(num)
+    print(num)
+
+print(Numbers)
+
+
+
+
+
