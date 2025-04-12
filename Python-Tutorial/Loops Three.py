8713f7021cb8a31aebd2c632ee53b5a4e762e0ad
Gary Yu <143667967+Yu2413@users.noreply.github.com>
2025-04-11 20:46:17 -0400

    Add files via upload

---
+++ Python-Tutorial/Loops Three.py
@@ -0,0 +1,36 @@
+Participants = ["Jen", "Alex", "Tina", "Joe", "Ben"]
+
+position = 1
+
+#for name in Participants:
+#   if name == "Alex":
+#        print("About to Break")
+#        break
+#    print("About to increment")
+#    position = position + 1
+
+#print(position)
+
+#Index = 0
+#for currentIndex in range(len(Participants)):
+#    print(currentIndex)
+#    if Participants[currentIndex] == "Joe":
+#        print("Have Breaked")
+#        break
+#    print("Not Breaked")
+#
+#print(currentIndex+1) #currentIndex plus 1
+
+for number in range(10):
+    if number%3 == 0:
+        print(number)
+        print("Divisible by 3")
+        continue
+    print(number)
+    print("Not Divisible by 3")
+
+
+
+
+
+
