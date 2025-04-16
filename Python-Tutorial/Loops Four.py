cd5d1c5b2de42438da2b175de8151576095e1a71
Gary Yu <143667967+Yu2413@users.noreply.github.com>
2025-04-15 20:41:23 -0400

    Add files via upload

---
+++ Python-Tutorial/Loops Four.py
@@ -0,0 +1,28 @@
+#Length = 10
+#for pos in range(1,Length+1):
+#    print("c"*pos)
+
+#Length = 10
+#ToPrint = "a"
+#for pos in range(1,Length+1):
+#    print(ToPrint*pos)
+
+# a Triangle
+
+#Length = 10
+#ToPrint = "b"
+#for pos in range(1,Length+1):
+#    print(ToPrint*pos)
+
+#b Triangle
+
+Length = 15
+ToPrint = "b"
+for pos in range(1,Length+1):
+    print(ToPrint*pos)
+for pos in range(Length-1,0,-1):
+    print(ToPrint*pos)
+
+# Full Triangle
+
+
